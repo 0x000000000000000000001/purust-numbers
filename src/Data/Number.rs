f8ae583fb2d7bfef39a5967b56df66cd5e52546a
0x1 <kevin.francart3@gmail.com>
2026-08-10 16:48:58 +0200

    misc

---
+++ src/Data/Number.rs
@@ -0,0 +1,7 @@
+
+pub fn Data_Number_isFinite() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Number_round() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Number_trunc() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Number_floor() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Number_ceil() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Number_abs() -> crate::UnknownType { crate::UnknownType::new(0) }
---
+++ src/Data/Number/Format.rs
@@ -0,0 +1,4 @@
+
+pub fn Data_Number_Format_toPrecisionNative() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Number_Format_toFixedNative() -> crate::UnknownType { crate::UnknownType::new(0) }
+pub fn Data_Number_Format_toExponentialNative() -> crate::UnknownType { crate::UnknownType::new(0) }
