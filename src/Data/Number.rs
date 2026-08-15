9c3bb88f04f4d663acd8bc99f8613d52e62e07c4
0x1 <kevin.francart3@gmail.com>
2026-08-15 19:05:23 +0200

    misc

--- src/Data/Number.rs
+++
@@ -1,6 +0,0 @@
-pub fn Data_Number_isFinite() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_Number_round() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_Number_trunc() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_Number_floor() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_Number_ceil() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_Number_abs() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
--- src/Data/Number/Format.rs
+++
@@ -1,4 +0,0 @@
-
-pub fn Data_Number_Format_toPrecisionNative() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_Number_Format_toFixedNative() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_Number_Format_toExponentialNative() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
